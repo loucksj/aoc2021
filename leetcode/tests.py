81926c83e697ac0cd8cf3ddeed7657c6a447d49d
loucksj <loucksj@gmail.com>
2021-04-23 20:55:23 -0700

    new test

--- leetcode/tests.py
+++ leetcode/tests.py
@@ -17,6 +17,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(False, p._9.isPalindrome(-121))
         self.assertEqual(False, p._9.isPalindrome(10))
         self.assertEqual(False, p._9.isPalindrome(-101))
+        self.assertEqual(False, p._9.isPalindrome(1000021))
 
     def test_167(self):
         self.assertEqual([1, 2], p._167.twoSum([2, 7, 11, 15], 9))

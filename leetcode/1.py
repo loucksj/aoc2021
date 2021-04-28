cc801382e0f67659ca8363f26ba59dd1b5e0a45e
loucksj <loucksj@gmail.com>
2021-04-28 11:31:36 -0700

    faster

--- leetcode/1.py
+++ leetcode/1.py
@@ -1,8 +1,11 @@
 def twoSums(nums: list[int], target: int) -> list[int]:
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+    d = {}
+
+    for i in range(0, len(nums)):
+        if nums[i] in d and nums[d[nums[i]]] + nums[i] == target:
+            return [d[nums[i]], i]    
+        else:
+            d[target - nums[i]] = i
 
 if __name__ == '__main__':
     assert twoSums([2, 7, 11, 15], 9) == [0, 1]

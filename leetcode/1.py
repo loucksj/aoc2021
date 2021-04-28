61daa6f66f84404a1daa6100c02b1636abc35657
loucksj <loucksj@gmail.com>
2021-04-28 11:50:02 -0700

    simplify, comments

--- leetcode/1.py
+++ leetcode/1.py
@@ -2,10 +2,10 @@ def twoSums(nums: list[int], target: int) -> list[int]:
     d = {}
 
     for i in range(0, len(nums)):
-        if nums[i] in d and nums[d[nums[i]]] + nums[i] == target:
-            return [d[nums[i]], i]    
+        if nums[i] in d:
+            return [d[nums[i]], i] # return saved index and this index
         else:
-            d[target - nums[i]] = i
+            d[target - nums[i]] = i # save this index at the other number
 
 if __name__ == '__main__':
     assert twoSums([2, 7, 11, 15], 9) == [0, 1]

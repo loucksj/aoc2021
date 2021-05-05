904a46f8c4dc21da808e2939d72c01f85940ff2c
loucksj <loucksj@gmail.com>
2021-05-05 13:10:01 -0700

    fast enough solution

--- leetcode/6.py
+++ leetcode/6.py
@@ -1,31 +1,32 @@
 def convert(s: str, numRows: int) -> str:
-    indexes = []
-
-    # create first row
-    space = max(1, 2 * numRows - 2) # distance between elements
-    i = 0
-    while i < len(s) + space:
-        indexes.append(i)
-        i += space
-
-    # create other rows
-    rows = 1
-    while rows < numRows:
-        indexes_copy = indexes
-        for i in indexes_copy:
-            h = i - 1
-            if h > 0 and not h in indexes:
-                indexes.append(h)
-            j = i + 1
-            if j < len(s) + space and not j in indexes:
-                indexes.append(j)
-        rows += 1
+    grid = []
+    for n in range(0, numRows):
+        grid.append([])
+    
+    row = 0
+    down = True
+    for i in range(0, len(s)):
+        grid[row].append(s[i])
+        if down and row < numRows - 1: 
+            # go down
+            row += 1
+        elif down and row == numRows - 1:
+            # at bottom, go up
+            down = False
+            row -= 1
+        elif not down and row > 0:
+            # go up
+            row -= 1
+        elif not down and row == 0:
+            # at top, go down
+            down = True
+            row += 1
     
     # create solution from indexes
     solution = ""
-    for i in indexes:
-        if i < len(s):
-            solution += s[i]
+    for row in grid:
+        for c in row:
+            solution += c
     
     return solution
 
@@ -34,4 +35,4 @@ if __name__ == '__main__':
     assert "PINALSIGYAHRPI" == convert("PAYPALISHIRING", 4)
     assert "A" == convert("A", 1)
     assert "ABDC" == convert("ABCD", 3)
-    # assert "ABDC" == convert("hjouvsuyoypayulyeimuotehzriicfskpggkbbipzzrzucxamludfykgruowzgiooobppleqlwphapjnadqhdcnvwdtxjbmyppphauxnspusgdhiixqmbfjxjcvudjsuyibyebmwsiqyoygyxymzevypzvjegebeocfuftsxdixtigsieehkchzdflilrjqfnxztqrsvbspkyhsenbppkqtpddbuotbbqcwivrfxjujjddntgeiqvdgaijvwcyaubwewpjvygehljxepbpiwuqzdzubdubzvafspqpqwuzifwovyddwyvvburczmgyjgfdxvtnunneslsplwuiupfxlzbknhkwppanltcfirjcddsozoyvegurfwcsfmoxeqmrjowrghwlkobmeahkgccnaehhsveymqpxhlrnunyfdzrhbasjeuygafoubutpnimuwfjqsjxvkqdorxxvrwctdsneogvbpkxlpgdirbfcriqifpgynkrrefx", 503)
+    assert convert("hjouvsuyoypayulyeimuotehzriicfskpggkbbipzzrzucxamludfykgruowzgiooobppleqlwphapjnadqhdcnvwdtxjbmyppphauxnspusgdhiixqmbfjxjcvudjsuyibyebmwsiqyoygyxymzevypzvjegebeocfuftsxdixtigsieehkchzdflilrjqfnxztqrsvbspkyhsenbppkqtpddbuotbbqcwivrfxjujjddntgeiqvdgaijvwcyaubwewpjvygehljxepbpiwuqzdzubdubzvafspqpqwuzifwovyddwyvvburczmgyjgfdxvtnunneslsplwuiupfxlzbknhkwppanltcfirjcddsozoyvegurfwcsfmoxeqmrjowrghwlkobmeahkgccnaehhsveymqpxhlrnunyfdzrhbasjeuygafoubutpnimuwfjqsjxvkqdorxxvrwctdsneogvbpkxlpgdirbfcriqifpgynkrrefx", 503)

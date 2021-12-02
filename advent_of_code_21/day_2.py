981759781ec6bc8e82cbbf0f8c2e5f03a877afe7
loucksj <loucksj@gmail.com>
2021-12-02 00:22:45 -0800

    day two part two done

--- advent_of_code_21/day_2.py
+++ advent_of_code_21/day_2.py
@@ -14,6 +14,27 @@ def part_1(file: str) -> list[int]:
             depth -= amount
     return horizontal * depth
 
+def part_2(file: str) -> list[int]:
+    lines = open(file, 'r').readlines()
+    horizontal = 0
+    depth = 0
+    aim = 0
+    for line in lines:
+        line = line.split()
+        direction = line[0]
+        amount = int(line[1])
+        if direction == "forward":
+            horizontal += amount
+            depth += amount * aim
+        if direction == "down":
+            aim += amount
+        if direction == "up":
+            aim -= amount
+    return horizontal * depth
+
 if __name__ == '__main__':
     assert part_1('day_2_test.txt') == 150
     assert part_1('day_2.txt') == 1480518
+
+    assert part_2('day_2_test.txt') == 900
+    assert part_2('day_2.txt') == 1282809906

2347454869c904d777ca133e1df0a49d22d64e8f
loucksj <loucksj@gmail.com>
2021-12-02 00:00:02 -0800

    part 2 complete

--- advent_of_code_21/day_1.py
+++ advent_of_code_21/day_1.py
@@ -15,6 +15,29 @@ def part_1(file: str) -> int:
         i += 1
     return count
 
+def part_2(file: str, size: int) -> int:
+    lines = open(file, 'r').readlines()
+    count = 0
+    index = 0
+    current = 0
+    previous = 0
+
+    for line in lines:
+        current += int(line)
+        if index >= size:
+            current -= int(lines[index-size])
+            if current > previous:
+                count += 1
+        previous = current
+        index += 1
+
+    return count
+
 if __name__ == '__main__':
     assert part_1('day_1_test.txt') == 7
     assert part_1('day_1.txt') == 1215
+
+    assert part_2('day_1_test.txt', 1) == 7
+    assert part_2('day_1.txt', 1) == 1215
+    assert part_2('day_1_test.txt', 3) == 5
+    assert part_2('day_1.txt', 3) == 1150

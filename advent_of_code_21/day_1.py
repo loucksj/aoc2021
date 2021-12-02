6583fa8304328ed741acb8828f5dc9423f9d03ff
loucksj <loucksj@gmail.com>
2021-12-01 22:55:32 -0800

    day 1 part 1 complete

--- advent_of_code_21/day_1.py
+++ advent_of_code_21/day_1.py
@@ -1,7 +1,20 @@
-def day_1() -> int:
+def part_1(file: str) -> int:
+    lines = open(file, 'r').readlines()
     count = 0
-
+    i = 0
+    current = 0
+    last = 0
+    for line in lines:
+        if i == 0:
+            current = int(line)
+        else:
+            last = current
+            current = int(line)
+            if current > last:
+                count += 1
+        i += 1
     return count
 
 if __name__ == '__main__':
-    assert day_1() == 0
+    assert part_1('day_1_test.txt') == 7
+    assert part_1('day_1.txt') == 1215

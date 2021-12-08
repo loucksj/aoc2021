962216960099d4ecaa737d8402a0d8725f6c7257
loucksj <loucksj@gmail.com>
2021-12-07 16:16:22 -0800

    day 4 initial commit

---
+++ advent_of_code_21/day_4.py
@@ -0,0 +1,10 @@
+def part_1(file: str) -> int:
+    lines = open(file, 'r').readlines()
+    return 0
+
+if __name__ == '__main__':
+    assert part_1('day_4_test.txt') == 0
+    #assert part_1('day_4.txt') == 0
+
+    #assert part_2('day_4_test.txt') == 0
+    #assert part_2('day_4.txt') == 0

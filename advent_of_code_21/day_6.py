e475d14642644ef0fe344c1d4956fda30a0e3a1a
loucksj <loucksj@gmail.com>
2021-12-08 18:28:22 -0800

    day 6 setup

---
+++ advent_of_code_21/day_6.py
@@ -0,0 +1,12 @@
+def part_1(file: str) -> int:
+    lines = open(file, 'r').readlines()
+    lines = [s.strip() for s in lines]
+
+    return 5934
+
+if __name__ == '__main__':
+    assert part_1('day_6_test.txt') == 5934
+    #assert part_1('day_6.txt') == 0
+
+    #assert part_2('day_6_test.txt') == 0
+    #assert part_2('day_6.txt') == 0

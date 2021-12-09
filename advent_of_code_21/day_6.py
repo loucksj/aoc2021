d634d09007d382facb80b04a9388e74ec83de324
loucksj <loucksj@gmail.com>
2021-12-08 20:37:54 -0800

    part one done, part two setup

--- advent_of_code_21/day_6.py
+++ advent_of_code_21/day_6.py
@@ -1,12 +1,40 @@
 def part_1(file: str) -> int:
     lines = open(file, 'r').readlines()
     lines = [s.strip() for s in lines]
+    
+    fish = list(map(int, lines[0].split(',')))
 
-    return 5934
+    for _ in range(0, 80): #80 days
+        count = len(fish)
+        for i in range(0, count):
+            if fish[i] == 0:
+                fish[i] = 6
+                fish.append(8)
+            else:
+                fish[i] -= 1
+
+    return len(fish)
+
+def part_2(file: str) -> int:
+    lines = open(file, 'r').readlines()
+    lines = [s.strip() for s in lines]
+    
+    fish = list(map(int, lines[0].split(',')))
+
+    for _ in range(0, 256):
+        count = len(fish)
+        for i in range(0, count):
+            if fish[i] == 0:
+                fish[i] = 6
+                fish.append(8)
+            else:
+                fish[i] -= 1
+
+    return len(fish)
 
 if __name__ == '__main__':
     assert part_1('day_6_test.txt') == 5934
-    #assert part_1('day_6.txt') == 0
+    assert part_1('day_6.txt') == 374994
 
-    #assert part_2('day_6_test.txt') == 0
+    #assert part_2('day_6_test.txt') == 26984457539
     #assert part_2('day_6.txt') == 0

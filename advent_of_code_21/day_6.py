d2fd0c7792077174844f4d989fd4c915fa5e4ef6
loucksj <loucksj@gmail.com>
2021-12-08 21:04:37 -0800

    part two done

--- advent_of_code_21/day_6.py
+++ advent_of_code_21/day_6.py
@@ -21,20 +21,27 @@ def part_2(file: str) -> int:
     
     fish = list(map(int, lines[0].split(',')))
 
-    for _ in range(0, 256):
-        count = len(fish)
-        for i in range(0, count):
-            if fish[i] == 0:
-                fish[i] = 6
-                fish.append(8)
-            else:
-                fish[i] -= 1
+    today = [0]*9
+    for i in range(0, 9):
+        for f in fish:
+            if f == i:
+                today[i] += 1
 
-    return len(fish)
+    for _ in range(0, 256):
+        tomor = [0]*9
+        for i in range(0, 9):
+            if i == 0:
+                tomor[8] = today[i]
+                tomor[6] = today[i]
+            else:
+                tomor[i-1] += today[i]
+        today = tomor
+
+    return sum(today)
 
 if __name__ == '__main__':
     assert part_1('day_6_test.txt') == 5934
     assert part_1('day_6.txt') == 374994
 
-    #assert part_2('day_6_test.txt') == 26984457539
-    #assert part_2('day_6.txt') == 0
+    assert part_2('day_6_test.txt') == 26984457539
+    assert part_2('day_6.txt') == 1686252324092

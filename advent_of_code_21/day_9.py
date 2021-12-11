51a838612a3c787fc877512c521a4362ad063deb
loucksj <loucksj@gmail.com>
2021-12-10 17:49:31 -0800

    part two done

--- advent_of_code_21/day_9.py
+++ advent_of_code_21/day_9.py
@@ -2,21 +2,51 @@ def part_1(file: str) -> int:
     lines = open(file, 'r').readlines()
     lines = [s.strip() for s in lines]
 
-    lows = get_lows(lines)
-
     total = 0
-    for low in lows:
+    for low in get_lows(lines):
         total += 1 + int(lines[low[0]][low[1]])
-
     return total
 
 def part_2(file: str) -> int:
     lines = open(file, 'r').readlines()
     lines = [s.strip() for s in lines]
 
-    lows = get_lows()
+    lows = get_lows(lines)
 
-    return 0
+    cells = []
+    for line in lines:
+        cells.append([int(x) for x in line])
+
+    sizes = []
+    for low in lows:
+        sizes.append(get_size(cells, low[0], low[1]))
+
+    sizes = sorted(sizes)
+
+    return sizes[-1] * sizes[-2] * sizes[-3]
+
+def get_size(cells: list, row: int, col: int) -> int:
+    count = 1
+    cells[row][col] = 0
+
+    if col > 0:
+        left = cells[row][col-1]
+        if left > 0 and left < 9:
+            count += get_size(cells, row, col-1)
+    if col < len(cells[0])-1:
+        right = cells[row][col+1]
+        if right > 0 and right < 9:
+            count += get_size(cells, row, col+1)
+    if row > 0:
+        up = cells[row-1][col]
+        if up > 0 and up < 9:
+            count += get_size(cells, row-1, col)
+    if row < len(cells)-1:
+        down = cells[row+1][col]
+        if down > 0 and down < 9:
+            count += get_size(cells, row+1, col)
+    
+    return count
 
 def get_lows(lines):
     width = len(lines[0])
@@ -40,5 +70,5 @@ if __name__ == '__main__':
     assert part_1('day_9_test.txt') == 15
     assert part_1('day_9.txt') == 522
 
-    #assert part_2('day_9_test.txt') == 1134
-    #assert part_2('day_9.txt') == 0
+    assert part_2('day_9_test.txt') == 1134
+    assert part_2('day_9.txt') == 916688

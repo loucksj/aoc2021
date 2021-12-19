743c03cee98764f42f1e9ed9e3f940053342603d
loucksj <loucksj@gmail.com>
2021-12-18 19:58:18 -0800

    distance refactored into rows

--- advent_of_code_21/day_15.py
+++ advent_of_code_21/day_15.py
@@ -9,34 +9,32 @@ def part_1(file: str) -> int:
 
 class Map():
     def __init__(self, lines: list):
-        self.point = [0, 0] #start
+        self.point = [0, 0]
         self.rows = []
-        self.distance = []
-        for line in lines:
-            row = []
-            for char in line:
-                row.append(int(char))
-            self.rows.append(row)
-            self.distance.append([-1]*len(self.rows[0]))
+        for i in range(0, len(lines)):
+            self.rows.append([])
+            for char in lines[i]:
+                self.rows[i].append([int(char), -1])
 
     def find_path(self):
-        end_point = [len(self.rows)-1, len(self.rows[0])-1]
-        self.distance[self.point[0]][self.point[1]] = 0
+        height = len(self.rows)
+        width = len(self.rows[0])
+        self.rows[0][0][1] = 0
         while True:
-            if self.point == end_point:
-                return self.distance[end_point[0]][end_point[1]]
+            if self.point[0] == height-1 and self.point[1] == width-1:
+                return self.rows[height-1][width-1][1]
             self.update()
-            self.distance[self.point[0]][self.point[1]] = 0
+            self.rows[self.point[0]][self.point[1]][1] = 0
             self.get_next()
 
     def get_next(self) -> list:
         smallest = 0
-        for row in range(0 , len(self.distance)):
-            for col in range(0, len(self.distance[0])):
-                d = self.distance[row][col]
+        for row in range(0 , len(self.rows)):
+            for col in range(0, len(self.rows[0])):
+                d = self.rows[row][col][1]
                 if d > 0 and (smallest == 0 or d < smallest):
                     self.point = [row, col]
-                    smallest = self.distance[self.point[0]][self.point[1]]
+                    smallest = self.rows[self.point[0]][self.point[1]][1]
 
     def update(self):
         directions = [[-1, 0], [+1, 0], [0, -1], [0, +1]] #up, down, left, right
@@ -45,14 +43,14 @@ class Map():
             col_end = self.point[1]+direction[1]
             if row_end < 0 or row_end >= len(self.rows) or col_end < 0 or col_end >= len(self.rows[0]):
                 continue #edge
-            if self.distance[row_end][col_end] == 0:
+            if self.rows[row_end][col_end][1] == 0:
                 continue #visited
-            d_start = self.distance[self.point[0]][self.point[1]]
-            d_end = self.distance[row_end][col_end]
-            end = self.rows[row_end][col_end]
+            d_start = self.rows[self.point[0]][self.point[1]][1]
+            d_end = self.rows[row_end][col_end][1]
+            end = self.rows[row_end][col_end][0]
             end_try = d_start + end
             if d_end == -1 or end_try < d_end:
-                self.distance[row_end][col_end] = end_try
+                self.rows[row_end][col_end][1] = end_try
 
 if __name__ == '__main__':
     assert part_1('day_15_test.txt') == 40

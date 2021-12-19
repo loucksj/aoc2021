a4422625bd3f725dc70f2e361ec5d5dba6a3174e
loucksj <loucksj@gmail.com>
2021-12-18 20:53:11 -0800

    faster

--- advent_of_code_21/day_15.py
+++ advent_of_code_21/day_15.py
@@ -11,6 +11,7 @@ class Map():
     def __init__(self, lines: list):
         self.point = [0, 0]
         self.rows = []
+        self.open = [[0, 0]]
         for i in range(0, len(lines)):
             self.rows.append([])
             for char in lines[i]:
@@ -21,20 +22,20 @@ class Map():
         width = len(self.rows[0])
         self.rows[0][0][1] = 0
         while True:
+            self.open.remove([self.point[0], self.point[1]])
+            self.update()
+            self.move()
             if self.point[0] == height-1 and self.point[1] == width-1:
                 return self.rows[height-1][width-1][1]
-            self.update()
-            self.rows[self.point[0]][self.point[1]][1] = 0
-            self.get_next()
 
-    def get_next(self) -> list:
+    def move(self) -> list:
+        self.rows[self.point[0]][self.point[1]][1] = 0
         smallest = 0
-        for row in range(0 , len(self.rows)):
-            for col in range(0, len(self.rows[0])):
-                d = self.rows[row][col][1]
-                if d > 0 and (smallest == 0 or d < smallest):
-                    self.point = [row, col]
-                    smallest = self.rows[self.point[0]][self.point[1]][1]
+        for point in self.open:
+            d = self.rows[point[0]][point[1]][1]
+            if d > 0 and (smallest == 0 or d < smallest):
+                self.point = [point[0], point[1]]
+                smallest = self.rows[self.point[0]][self.point[1]][1]
 
     def update(self):
         directions = [[-1, 0], [+1, 0], [0, -1], [0, +1]] #up, down, left, right
@@ -48,6 +49,8 @@ class Map():
                 continue #visited
             distance = self.rows[row][col][0] + self.rows[self.point[0]][self.point[1]][1]
             if risk == -1 or distance < risk:
+                if risk == -1:
+                    self.open.append([row, col])
                 self.rows[row][col][1] = distance
 
 if __name__ == '__main__':

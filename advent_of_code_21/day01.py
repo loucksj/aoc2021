a2aa74a54613da9ca25d6cd15e27584ed5afe969
loucksj <loucksj@gmail.com>
2021-12-22 21:27:58 -0800

    Sonar encapsulated

--- advent_of_code_21/day01.py
+++ advent_of_code_21/day01.py
@@ -1,51 +1,42 @@
 from main import Reader
 
-def part_1(filename: str) -> int:
+
+def part01(filename: str) -> int:
     sonar = Sonar(filename)
-    count = 0
-    i = 0
-    current = 0
-    last = 0
-    for line in sonar.sweep:
-        if i == 0:
-            current = line
-        else:
-            last = current
-            current = line
-            if current > last:
-                count += 1
-        i += 1
-    return count
+    increases = sonar.increaseCount(width=1)
+    return increases
+
+
+def part02(filename: str) -> int:
+    sonar = Sonar(filename)
+    increases = sonar.increaseCount(width=3)
+    return increases
+
 
 class Sonar():
     def __init__(self, filename: str):
         reader = Reader(filename)
         self.sweep = reader.getLinesAsInts()
 
-def part_2(file: str, size: int) -> int:
-    lines = open(file, 'r').readlines()
-    count = 0
-    index = 0
-    current = 0
-    previous = 0
-
-    for line in lines:
-        current += int(line)
-        if index >= size:
-            current -= int(lines[index-size])
-            if current > previous:
-                count += 1
-        previous = current
-        index += 1
-
-    return count
+    def increaseCount(self, width: int) -> int:
+        count = 0
+        index = 0
+        current = 0
+        previous = 0
+        for line in self.sweep:
+            current += line
+            if index >= width:
+                current -= self.sweep[index-width]
+                if current > previous:
+                    count += 1
+            previous = current
+            index += 1
+        return count
 
 
 if __name__ == '__main__':
-    assert part_1('day01input_example.txt') == 7
-    assert part_1('day01input.txt') == 1215
+    assert part01('day01input_example.txt') == 7
+    assert part01('day01input.txt') == 1215
 
-    assert part_2('day01input_example.txt', 1) == 7
-    assert part_2('day01input.txt', 1) == 1215
-    assert part_2('day01input_example.txt', 3) == 5
-    assert part_2('day01input.txt', 3) == 1150
+    assert part02('day01input_example.txt') == 5
+    assert part02('day01input.txt') == 1150

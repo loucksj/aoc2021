53255de19c266275f5297ad9672fdf656149abe0
loucksj <loucksj@gmail.com>
2021-12-23 23:30:04 -0800

    moved shared movement to base Submarine

--- advent_of_code_21/day02.py
+++ advent_of_code_21/day02.py
@@ -1,5 +1,6 @@
 from main import Reader
 
+
 def part_1(filename: str) -> int:
     submarine = HorizontalSubmarine()
     submarine.navigate_from_file(filename)
@@ -11,14 +12,18 @@ def part_2(filename: str) -> int:
     submarine.navigate_from_file(filename)
     return submarine.horizontal * submarine.depth
 
+
 class Submarine():
     def __init__(self):
         self.depth = 0
         self.horizontal = 0
-    
+        self.aim = 0
+
     def navigate_from_file(self, filename: str):
-        directions = [line.split(' ') for line in Reader(filename).getLinesStripped()]
-        directions = [[direction, int(magnitude)] for direction, magnitude in directions]
+        directions = [line.split(' ')
+                      for line in Reader(filename).getLinesStripped()]
+        directions = [[direction, int(magnitude)]
+                      for direction, magnitude in directions]
         self.navigate(directions)
 
     def navigate(self, directions: list):
@@ -31,30 +36,26 @@ class Submarine():
                 case "up":
                     self.up(magnitude)
 
-class HorizontalSubmarine(Submarine):   
     def forward(self, magnitude):
         self.horizontal += magnitude
+        self.depth += magnitude * self.aim
 
+
+class HorizontalSubmarine(Submarine):
     def up(self, magnitude):
         self.depth -= magnitude
 
     def down(self, magnitude):
         self.depth += magnitude
 
-class AimSubmarine(Submarine):
-    def __init__(self):
-        super().__init__()
-        self.aim = 0
-            
-    def forward(self, magnitude):
-        self.horizontal += magnitude
-        self.depth += magnitude * self.aim
 
+class AimSubmarine(Submarine):
     def up(self, magnitude):
         self.aim -= magnitude
 
     def down(self, magnitude):
         self.aim += magnitude
+
 
 if __name__ == '__main__':
     assert part_1('day02_example.txt') == 150

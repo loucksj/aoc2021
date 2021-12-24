ab0dd52b32a5a1faa92486468865e2ab70d5e8a6
loucksj <loucksj@gmail.com>
2021-12-24 12:20:18 -0800

    leetcode removed

--- leetcode/69.py
+++
@@ -1,9 +0,0 @@
-def mySqrt(x: int) -> int:
-    return int(x**(1/2))
-
-if __name__ == '__main__':
-    assert 2 == mySqrt(4)
-    assert 2 == mySqrt(8)
-    assert 1 == mySqrt(1)
-    assert 3 == mySqrt(9)
-    assert 1 == mySqrt(2)

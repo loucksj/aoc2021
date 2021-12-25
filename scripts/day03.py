e10b9ea3f71b0c52bf6ebcf98ed593178a24008e
loucksj <loucksj@gmail.com>
2021-12-25 10:59:20 -0800

    Binaries class removed

--- scripts/day03.py
+++ scripts/day03.py
@@ -1,59 +1,46 @@
-from scripts.input_manager import char_lines, strip_lines
+from scripts.input_manager import char_lines
 
 
 def part_one(filename: str) -> int:
-    binary = majority_binary(char_lines(filename))
-    return int(binary, 2) * int(flip(binary), 2)
+    digits = majority_binary(char_lines(filename))
+    return binary_digits_int(digits) * binary_digits_int(flip_digits(digits))
 
 
 def part_two(filename: str) -> int:
-    major_binaries = Binaries().from_file(filename)
-    minor_binaries = Binaries().from_file(filename)
-    return int(major_binaries.major_path(), 2) * int(minor_binaries.minor_path(), 2)
+    digits = char_lines(filename)
+    return binary_digits_int(major_path(digits)) * binary_digits_int(minor_path(digits))
 
-def majority_binary(values: list):
-    return ''.join([commonest_element(line) for line in transpose(values)])
+def binary_digits_int(digits: list):
+    return int(''.join(digits), 2)
 
-def commonest_element(values: list):
-    return max(set(values), key=values.count)
+def majority_binary(binaries: list):
+    return [commonest_element(column) for column in transpose(binaries)]
 
-def transpose(values: list):
-    return [list(x) for x in zip(*values)]
+def commonest_element(binary: list) -> str:
+    return '1' if binary.count('1') >= binary.count('0') else '0'
 
-class Binaries:
-    def from_file(self, filename: str):
-        self.binaries = strip_lines(filename)
-        self.digits = len(self.binaries[0])
-        return self
+def transpose(binaries: list):
+    return [list(x) for x in zip(*binaries)]
 
-    def majority_bit(self, i: int) -> str:
-        return '1' if self.sum_index(i) >= len(self.binaries)/2 else '0'
+def major_path(binaries: list) -> str:
+    i = 0
+    while len(binaries) > 1:
+        binaries = majority_elements(binaries, i)
+        i += 1
+    return binaries[0]
 
-    def sum_index(self, i: int) -> int:
-        return sum(int(binary[i]) for binary in self.binaries)
+def minor_path(binaries) -> str:
+    i = 0
+    while len(binaries) > 1:
+        binaries = minority_elements(binaries, i)
+        i += 1
+    return binaries[0]
 
-    def major_path(self) -> str:
-        i = 0
-        while len(self.binaries) > 1:
-            self.binaries = self.index_majorities(i)
-            i += 1
-        return self.binaries[0]
+def majority_elements(binaries: list, i: int) -> list:
+    return [binary for binary in binaries if binary[i] == majority_binary(binaries)[i]]
 
-    def minor_path(self) -> str:
-        i = 0
-        while len(self.binaries) > 1:
-            self.binaries = self.index_minorities(i)
-            i += 1
-        return self.binaries[0]
+def minority_elements(binaries, i: int) -> list:
+    return [binary for binary in binaries if binary[i] != majority_binary(binaries)[i]]
 
-    def index_majorities(self, i: int) -> list:
-        majority = self.majority_bit(i)
-        return [binary for binary in self.binaries if binary[i] == majority]
-
-    def index_minorities(self, i: int) -> list:
-        majority = self.majority_bit(i)
-        return [binary for binary in self.binaries if binary[i] != majority]
-
-
-def flip(binary: str) -> str:
-    return ''.join(['1' if i == '0' else '0' for i in binary])
+def flip_digits(binary: list) -> str:
+    return ['1' if digit == '0' else '0' for digit in binary]

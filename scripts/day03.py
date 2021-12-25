bc16439d0e15a21a0af34e89011a33fdd229594a
loucksj <loucksj@gmail.com>
2021-12-24 23:23:34 -0800

    index_sums removed, using sum_index

--- scripts/day03.py
+++ scripts/day03.py
@@ -1,5 +1,6 @@
 from scripts.input_manager import strip_lines
 
+
 def part_one(filename: str) -> int:
     majority = majority_bits(strip_lines(filename))
     return int(majority, 2) * int(flip(majority), 2)
@@ -18,18 +19,13 @@ def majority_bits(binaries: list) -> str:
     return ''.join([majority_bit(binaries, i) for i in range(len(binaries[0]))])
 
 
-def majority_bit(binaries: list, index: int) -> str:
-    return '1' if index_sums(binaries)[index] >= len(binaries)/2 else '0'
+def majority_bit(binaries: list, i: int) -> str:
+    return '1' if sum_index(binaries, i) >= len(binaries)/2 else '0'
 
-
-def index_sums(values: list) -> list:
-    sum_by_index = [0]*len(values[0])
-    for i in range(len(values[0])):
-        sum_by_index[i] += sum_index(values, i)
-    return sum_by_index
 
 def sum_index(values: list, i: int) -> int:
     return sum(int(value[i]) for value in values)
+
 
 def major_path(binaries: list) -> str:
     i = 0
@@ -38,6 +34,7 @@ def major_path(binaries: list) -> str:
         i += 1
     return binaries[0]
 
+
 def minor_path(binaries: list) -> str:
     i = 0
     while len(binaries) > 1:
@@ -45,9 +42,11 @@ def minor_path(binaries: list) -> str:
         i += 1
     return binaries[0]
 
+
 def index_majorities(binaries: list, i: int) -> list:
     majority = majority_bit(binaries, i)
     return [binary for binary in binaries if binary[i] == majority]
+
 
 def index_minorities(binaries: list, i: int) -> list:
     majority = majority_bit(binaries, i)

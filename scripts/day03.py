21c1f0773e130147cbbf6a670282e26bb8c2a10c
loucksj <loucksj@gmail.com>
2021-12-25 15:50:10 -0800

    path refactor and reduced

--- scripts/day03.py
+++ scripts/day03.py
@@ -2,13 +2,13 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    majority = Bitmatrix().from_file(filename).majority_per_index()
+    majority = Bitmatrix().from_file(filename).majority_bits()
     return as_int(majority) * as_int(flip(majority))
 
 
 def part_two(filename: str) -> int:
     bitmatrix = Bitmatrix().from_file(filename)
-    return as_int(bitmatrix.most_path()) * as_int(bitmatrix.least_path())
+    return as_int(bitmatrix.path(True)) * as_int(bitmatrix.path(False))
 
 
 def as_int(bits: list):
@@ -31,31 +31,17 @@ class Bitmatrix():
         self.matrix = Reader(filename).matrix()
         return self
 
-    def majority_per_index(self) -> list:
+    def majority_bits(self) -> list:
         return [most(column) for column in self.transposed()]
 
     def transposed(self):
         return [list(x) for x in zip(*self.matrix)]
 
-    def most_path(self) -> list:
-        return self.path(True)
-    
-    def least_path(self) -> list:
-        return self.path(False)
-
-    def path(self, most):
+    def path(self, up):
         i = 0
         sift = Bitmatrix(self.matrix)
         while len(sift.matrix) > 1:
-            if most:
-                sift.matrix = sift.majority_at_index(i)
-            else:
-                sift.matrix = sift.minority_at_index(i)
+            sift.matrix = [bits for bits in sift.matrix if up ==
+                           (bits[i] == sift.majority_bits()[i])]
             i += 1
         return sift.matrix[0]
-
-    def majority_at_index(self, i: int) -> list:
-        return [bits for bits in self.matrix if bits[i] == self.majority_per_index()[i]]
-
-    def minority_at_index(self, i: int) -> list:
-        return [bits for bits in self.matrix if bits[i] != self.majority_per_index()[i]]

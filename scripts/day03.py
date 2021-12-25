650c420aa81a4fa3c4f8fc9e408ad8b6fdd71917
loucksj <loucksj@gmail.com>
2021-12-25 10:59:49 -0800

    formatted

--- scripts/day03.py
+++ scripts/day03.py
@@ -10,17 +10,22 @@ def part_two(filename: str) -> int:
     digits = char_lines(filename)
     return binary_digits_int(major_path(digits)) * binary_digits_int(minor_path(digits))
 
+
 def binary_digits_int(digits: list):
     return int(''.join(digits), 2)
+
 
 def majority_binary(binaries: list):
     return [commonest_element(column) for column in transpose(binaries)]
 
+
 def commonest_element(binary: list) -> str:
     return '1' if binary.count('1') >= binary.count('0') else '0'
 
+
 def transpose(binaries: list):
     return [list(x) for x in zip(*binaries)]
+
 
 def major_path(binaries: list) -> str:
     i = 0
@@ -29,6 +34,7 @@ def major_path(binaries: list) -> str:
         i += 1
     return binaries[0]
 
+
 def minor_path(binaries) -> str:
     i = 0
     while len(binaries) > 1:
@@ -36,11 +42,14 @@ def minor_path(binaries) -> str:
         i += 1
     return binaries[0]
 
+
 def majority_elements(binaries: list, i: int) -> list:
     return [binary for binary in binaries if binary[i] == majority_binary(binaries)[i]]
 
+
 def minority_elements(binaries, i: int) -> list:
     return [binary for binary in binaries if binary[i] != majority_binary(binaries)[i]]
+
 
 def flip_digits(binary: list) -> str:
     return ['1' if digit == '0' else '0' for digit in binary]

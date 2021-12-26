67d4e5443e939c8ed5f82776bb954c7eb88aecff
loucksj <loucksj@gmail.com>
2021-12-25 20:39:59 -0800

    ordering and naming

--- scripts/day03.py
+++ scripts/day03.py
@@ -2,13 +2,13 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    bits = majority(Reader(filename).matrix())
+    bits = average_columns(Reader(filename).matrix())
     return as_int(bits) * as_int(flip(bits))
 
 
 def part_two(filename: str) -> int:
     bitmatrix = Reader(filename).matrix()
-    return as_int(average_bits(bitmatrix)) * as_int(average_bits(bitmatrix, opposite=True))
+    return as_int(average_seive(bitmatrix)) * as_int(average_seive(bitmatrix, invert=True))
 
 
 def as_int(bits: list) -> int:
@@ -19,24 +19,25 @@ def flip(bits: list) -> list:
     return ['1' if digit == '0' else '0' for digit in bits]
 
 
-def most(bits: list) -> str:
+def average_seive(bitmatrix: list, invert=False) -> list:
+    matrix = bitmatrix[:]
+    i = 0
+    while len(matrix) > 1:
+        check = average_columns(matrix) if not invert else flip(
+            average_columns(matrix))
+        matrix = [bits for bits in matrix if bits[i] == check[i]]
+        i += 1
+    return matrix[0]
+
+
+def average_columns(bitmatrix: list) -> list:
+    return [average_bit(column) for column in transposed(bitmatrix)]
+
+
+def average_bit(bits: list) -> str:
+    # spec: tie favors '1'
     return '1' if bits.count('1') >= bits.count('0') else '0'
 
 
-def majority(bitmatrix: list) -> list:
-    return [most(column) for column in transposed(bitmatrix)]
-
-
 def transposed(bitmatrix: list) -> list:
-    return [list(x) for x in zip(*bitmatrix)]
-
-
-def average_bits(bitmatrix: list, opposite=False) -> list:
-    filtered = bitmatrix.copy()
-    i = 0
-    while len(filtered) > 1:
-        targets = majority(filtered) if not opposite else flip(
-            majority(filtered))
-        filtered = [bits for bits in filtered if bits[i] == targets[i]]
-        i += 1
-    return filtered[0]
+    return list(map(list, zip(*bitmatrix)))

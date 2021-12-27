6b4dcbe8e6d25f72d4ca1cdd7bc65f63a89173f4
loucksj <loucksj@gmail.com>
2021-12-26 20:38:51 -0800

    BingoGame class

--- scripts/day04.py
+++ scripts/day04.py
@@ -2,64 +2,62 @@ from scripts.main import Reader, transpose
 
 
 def part_one(filename: str) -> int:
-    draws = draws_from_file(filename)
-    boards = boards_from_file(filename)
-    winners, draw = winners_at(boards, draws)
+    game = BingoGame().from_file(filename)
+    winners, draw = game.winners_at()
     return draw * winners[0].score()
 
 
 def part_two(filename: str) -> int:
-    draws = draws_from_file(filename)
-    boards = boards_from_file(filename)
-    loser, draw = loser_at(boards, draws)
+    game = BingoGame().from_file(filename)
+    loser, draw = game.loser_at()
     return draw * loser.score()
 
 
-def draws_from_file(filename: str) -> list:
-    return [int(s) for s in Reader(filename).lines()[0].split(',')]
+class BingoGame():
+    def from_file(self, filename: str):
+        self.draws = self.draws_from_file(filename)
+        self.boards = self.boards_from_file(filename)
+        return self
 
+    def draws_from_file(self, filename: str) -> list:
+        return [int(s) for s in Reader(filename).lines()[0].split(',')]
 
-def boards_from_file(filename: str) -> list:
-    str_boards = Reader(filename).read().split('\n\n')[1:]
-    int_boards = [[map(int, line.split())
-                   for line in board.split('\n')] for board in str_boards]
-    return [Board(board) for board in int_boards]
+    def boards_from_file(self, filename: str) -> list:
+        str_boards = Reader(filename).read().split('\n\n')[1:]
+        int_boards = [[map(int, line.split())
+                       for line in board.split('\n')] for board in str_boards]
+        return [Board(board) for board in int_boards]
 
+    def winners_at(self) -> tuple:
+        for draw in self.draws:
+            self.mark_all(draw)
+            if len(self.winners()) > 0:
+                return self.winners(), draw
 
-def winners_at(boards: list, draws: list) -> tuple:
-    for draw in draws:
-        mark_all(boards, draw)
-        if len(winners(boards)) > 0:
-            return winners(boards), draw
+    def winners(self):
+        return [board for board in self.boards if board.is_winner()]
 
+    def loser_at(self) -> tuple:
+        last_draw = 0
+        last_board = []
+        for draw in self.draws:
+            self.mark_all(draw)
+            if len(self.winners()) > 0:
+                last_board = self.winners()[-1]
+                last_draw = draw
+            self.remove_winners()
+            if len(self.boards) == 0:
+                break
+        return last_board, last_draw
 
-def winners(boards: list):
-    return [board for board in boards if board.is_winner()]
+    def remove_winners(self):
+        for board in self.boards:
+            if board.is_winner():
+                self.boards.remove(board)
 
-
-def loser_at(boards: list, draws: list) -> tuple:
-    last_draw = 0
-    last_board = []
-    for draw in draws:
-        mark_all(boards, draw)
-        if len(winners(boards)) > 0:
-            last_board = winners(boards)[-1]
-            last_draw = draw
-        remove_winners(boards)
-        if len(boards) == 0:
-            break
-    return last_board, last_draw
-
-
-def remove_winners(boards: list):
-    for board in boards:
-        if board.is_winner():
-            boards.remove(board)
-
-
-def mark_all(boards: list, draw: int):
-    for board in boards:
-        board.mark(draw)
+    def mark_all(self, draw: int):
+        for board in self.boards:
+            board.mark(draw)
 
 
 class Board:

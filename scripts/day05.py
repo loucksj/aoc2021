1829ebf1e9911169a627862c275e539c45692b32
loucksj <loucksj@gmail.com>
2021-12-27 02:57:55 -0800

    coordinates brought into Vents

--- scripts/day05.py
+++ scripts/day05.py
@@ -2,34 +2,32 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    coordinates = coordinates_from_file(filename)
-    vents = Vents(get_size(coordinates))
-
-    vents.mark_vertical(coordinates)
-    vents.mark_horizontal(coordinates)
-
+    vents = Vents(filename)
+    vents.mark_vertical()
+    vents.mark_horizontal()
     return vents.score()
 
 
 def part_two(filename: str) -> int:
-    coordinates = coordinates_from_file(filename)
-    vents = Vents(get_size(coordinates))
-
-    vents.mark_vertical(coordinates)
-    vents.mark_horizontal(coordinates)
-    vents.mark_down_diagonal(coordinates)
-    vents.mark_up_diagonal(coordinates)
-
+    vents = Vents(filename)
+    vents.mark_vents()
     return vents.score()
 
 class Vents:
-    def __init__(self, size: int):
+    def __init__(self, filename: str):
+        self.coordinates = self.coordinates_from_file(filename)
         self.rows = []
-        for _ in range(0, size):
-            self.rows.append([0]*size)
+        for _ in range(0, self.get_size()):
+            self.rows.append([0]*self.get_size())
+    
+    def mark_vents(self):
+        self.mark_vertical()
+        self.mark_horizontal()
+        self.mark_down_diagonal()
+        self.mark_up_diagonal()
 
-    def mark_vertical(self, coordinates: list):
-        for pair in coordinates:
+    def mark_vertical(self):
+        for pair in self.coordinates:
             start = pair[0]
             end = pair[1]
             if start[0] == end[0]:
@@ -39,8 +37,8 @@ class Vents:
                 for i in range(0, diff+1):
                     self.rows[row+i][col] += 1
 
-    def mark_horizontal(self, coordinates: list):
-        for pair in coordinates:
+    def mark_horizontal(self):
+        for pair in self.coordinates:
             start = pair[0]
             end = pair[1]
             if start[1] == end[1]:
@@ -50,8 +48,8 @@ class Vents:
                 for i in range(0, diff+1):
                     self.rows[row][col+i] += 1
 
-    def mark_down_diagonal(self, coordinates: list):
-        for pair in coordinates:
+    def mark_down_diagonal(self):
+        for pair in self.coordinates:
             start = pair[0]
             end = pair[1]
             if start[0] - end[0] == start[1] - end[1]:
@@ -61,8 +59,8 @@ class Vents:
                 for i in range(0, diff+1):
                     self.rows[row+i][col+i] += 1
 
-    def mark_up_diagonal(self, coordinates: list):
-        for pair in coordinates:
+    def mark_up_diagonal(self):
+        for pair in self.coordinates:
             start = pair[0]
             end = pair[1]
             if start[0] - end[0] == -(start[1] - end[1]):
@@ -81,14 +79,13 @@ class Vents:
         return total
 
 
-def get_size(coordinates: list) -> list:
-    size = [0, 0]
-    for pair in coordinates:
-        for xy in pair:
-            size[0] = max(size[0], xy[0])
-            size[1] = max(size[1], xy[1])
-    return max(size) + 1
+    def get_size(self) -> list:
+        size = [0, 0]
+        for pair in self.coordinates:
+            for xy in pair:
+                size[0] = max(size[0], xy[0])
+                size[1] = max(size[1], xy[1])
+        return max(size) + 1
 
-
-def coordinates_from_file(filename: str) -> list:
-    return [[list(map(int, pair.split(','))) for pair in coordinate] for coordinate in Reader(filename).split_lines(' -> ')]
+    def coordinates_from_file(self, filename: str) -> list:
+        return [[list(map(int, pair.split(','))) for pair in coordinate] for coordinate in Reader(filename).split_lines(' -> ')]

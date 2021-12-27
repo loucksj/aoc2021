b11596fd85300de5501e5b74649a49d234f0e199
loucksj <loucksj@gmail.com>
2021-12-27 03:27:28 -0800

    row initialize func

--- scripts/day05.py
+++ scripts/day05.py
@@ -13,14 +13,19 @@ def part_two(filename: str) -> int:
     vents.mark_vents()
     return vents.score()
 
+
 class Vents:
     def __init__(self, filename: str):
-        self.coordinates = self.coordinates_from_file(filename)
-        self.rows = []
-        maximums = self.get_max_xy()
-        for _ in range(maximums[0]+1):
-            self.rows.append([0]*(maximums[1]+1))
-    
+        self.paths = self.paths_from_file(filename)
+        self.rows = self.zero_rows()
+
+    def zero_rows(self):
+        rows = []
+        max_x, max_y = self.get_max_xy()
+        for _ in range(max_x+1):
+            rows.append([0]*(max_y+1))
+        return rows
+
     def mark_vents(self):
         self.mark_vertical()
         self.mark_horizontal()
@@ -28,7 +33,7 @@ class Vents:
         self.mark_up_diagonal()
 
     def mark_vertical(self):
-        for pair in self.coordinates:
+        for pair in self.paths:
             start = pair[0]
             end = pair[1]
             if start[0] == end[0]:
@@ -39,7 +44,7 @@ class Vents:
                     self.rows[row+i][col] += 1
 
     def mark_horizontal(self):
-        for pair in self.coordinates:
+        for pair in self.paths:
             start = pair[0]
             end = pair[1]
             if start[1] == end[1]:
@@ -50,7 +55,7 @@ class Vents:
                     self.rows[row][col+i] += 1
 
     def mark_down_diagonal(self):
-        for pair in self.coordinates:
+        for pair in self.paths:
             start = pair[0]
             end = pair[1]
             if start[0] - end[0] == start[1] - end[1]:
@@ -61,7 +66,7 @@ class Vents:
                     self.rows[row+i][col+i] += 1
 
     def mark_up_diagonal(self):
-        for pair in self.coordinates:
+        for pair in self.paths:
             start = pair[0]
             end = pair[1]
             if start[0] - end[0] == -(start[1] - end[1]):
@@ -74,14 +79,13 @@ class Vents:
     def score(self) -> int:
         return sum([sum([1 if num > 1 else 0 for num in row]) for row in self.rows])
 
-
     def get_max_xy(self) -> tuple:
-        max_xy = [0, 0]
-        for pair in self.coordinates:
+        max_x, max_y = 0, 0
+        for pair in self.paths:
             for x, y in pair:
-                max_xy[0] = max(max_xy[0], x)
-                max_xy[1] = max(max_xy[1], y)
-        return (max_xy[0], max_xy[1])
+                max_x = max(max_x, x)
+                max_y = max(max_y, y)
+        return (max_x, max_y)
 
-    def coordinates_from_file(self, filename: str) -> list:
-        return [[list(map(int, pair.split(','))) for pair in coordinate] for coordinate in Reader(filename).split_lines(' -> ')]
+    def paths_from_file(self, filename: str) -> list:
+        return [[list(map(int, pair.split(','))) for pair in path] for path in Reader(filename).split_lines(' -> ')]

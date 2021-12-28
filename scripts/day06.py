0a5dc855e8ad35b3419a0a8dd6ae60ce49da5a5d
loucksj <loucksj@gmail.com>
2021-12-27 22:31:12 -0800

    added Fishes class

--- scripts/day06.py
+++ scripts/day06.py
@@ -2,24 +2,23 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    return fish_after_days(get_fish_from_file(filename), 80)
+    return Fishes(filename).after_days(80)
 
 
 def part_two(filename: str) -> int:
-    return fish_after_days(get_fish_from_file(filename), 256)
+    return Fishes(filename).after_days(256)
 
+class Fishes():
+    def __init__(self, filename: str) -> None:
+        fishes = Reader(filename).split_firstline_ints(',')
+        self.fish = [fishes.count(i) for i in range(10)]
 
-def fish_after_days(fish: list, days: int) -> int:
-    for _ in range(days):
-        next_fish = [0]*9
-        next_fish[8] = fish[0]
-        next_fish[6] = fish[0]
-        for i in range(1, 9):
-            next_fish[i-1] += fish[i]
-        fish = next_fish
-    return sum(fish)
-
-
-def get_fish_from_file(filename: str) -> list:
-    fishes = Reader(filename).split_firstline_ints(',')
-    return [fishes.count(i) for i in range(10)]
+    def after_days(self, days: int) -> int:
+        for _ in range(days):
+            next_fish = [0]*9
+            next_fish[8] = self.fish[0]
+            next_fish[6] = self.fish[0]
+            for i in range(1, 9):
+                next_fish[i-1] += self.fish[i]
+            self.fish = next_fish
+        return sum(self.fish)

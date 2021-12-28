3b322c2ea84a7092f2b70e8d02b271e920479cb3
loucksj <loucksj@gmail.com>
2021-12-27 22:36:25 -0800

    Fishes class improved with next_day

--- scripts/day06.py
+++ scripts/day06.py
@@ -2,23 +2,26 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    return Fishes(filename).after_days(80)
+    return Fishes(filename).after(80)
 
 
 def part_two(filename: str) -> int:
-    return Fishes(filename).after_days(256)
+    return Fishes(filename).after(256)
 
 class Fishes():
     def __init__(self, filename: str) -> None:
         fishes = Reader(filename).split_firstline_ints(',')
         self.fish = [fishes.count(i) for i in range(10)]
 
-    def after_days(self, days: int) -> int:
+    def after(self, days: int) -> int:
         for _ in range(days):
-            next_fish = [0]*9
-            next_fish[8] = self.fish[0]
-            next_fish[6] = self.fish[0]
-            for i in range(1, 9):
-                next_fish[i-1] += self.fish[i]
-            self.fish = next_fish
+            self.next_day()
         return sum(self.fish)
+    
+    def next_day(self):
+        fish = [0]*9
+        fish[8] = self.fish[0]
+        fish[6] = self.fish[0]
+        for i in range(1, 9):
+            fish[i-1] += self.fish[i]
+        self.fish = fish

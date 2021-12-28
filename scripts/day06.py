1b79e88b25c75e857814af88bc37ba4d8e5e9d7d
loucksj <loucksj@gmail.com>
2021-12-27 22:47:20 -0800

    spacing

--- scripts/day06.py
+++ scripts/day06.py
@@ -8,6 +8,7 @@ def part_one(filename: str) -> int:
 def part_two(filename: str) -> int:
     return Fishes(filename).after(256)
 
+
 class Fishes():
     def __init__(self, filename: str) -> None:
         fishes = Reader(filename).split_firstline_ints(',')
@@ -17,7 +18,7 @@ class Fishes():
         for _ in range(days):
             self.next_day()
         return sum(self.fish)
-    
+
     def next_day(self):
         birth = self.fish.pop(0)
         self.fish.append(birth)

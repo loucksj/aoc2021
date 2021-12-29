6b438e1f78666faf3a7098ca7b3016ac1d20b389
loucksj <loucksj@gmail.com>
2021-12-28 23:03:41 -0800

    formatting

--- scripts/day08.py
+++ scripts/day08.py
@@ -1,65 +1,58 @@
 from scripts.main import Reader
 
+
 def part_one(filename: str) -> int:
     lines = Reader(filename).lines()
-    
     pairs = get_pairs(lines)
-
     count = 0
     for pair in pairs:
         output = pair[1]
         for digit in output.split():
             s = len(digit)
             # 1, 4, 7, 8
-            if s==2 or s==3 or s==4 or s==7:
+            if s == 2 or s == 3 or s == 4 or s == 7:
                 count += 1
-
     return count
+
 
 def part_two(filename: str) -> int:
     lines = Reader(filename).lines()
-    
     pairs = get_pairs(lines)
-
     total = 0
     for pair in pairs:
         outputs = pair[1].split()
-        
         decoded = decode(pair[0].split())
-
         value = 0
         for digit in outputs:
             digit = "".join(sorted(digit))
             value *= 10
             value += decoded[digit]
         total += value
-
     return total
+
 
 def decode(signals: list) -> dict:
     decoded = {}
-
     for i in range(0, len(signals)):
         signals[i] = "".join(sorted(signals[i]))
-
     # 1, 4, 7, 8
     for digit in signals:
-        if len(digit)==2:
+        if len(digit) == 2:
             decoded[1] = digit
             signals.remove(digit)
             break
     for digit in signals:
-        if len(digit)==3:
+        if len(digit) == 3:
             decoded[7] = digit
             signals.remove(digit)
             break
     for digit in signals:
-        if len(digit)==4:
+        if len(digit) == 4:
             decoded[4] = digit
             signals.remove(digit)
             break
     for digit in signals:
-        if len(digit)==7:
+        if len(digit) == 7:
             decoded[8] = digit
             signals.remove(digit)
             break
@@ -91,7 +84,6 @@ def decode(signals: list) -> dict:
                 decoded[5] = digit
                 signals.remove(digit)
                 break
-
     # 2, 3
     for digit in signals:
         if len(digit) == 5:
@@ -116,6 +108,7 @@ def decode(signals: list) -> dict:
     decoded[9] = signals.pop()
     decoded = {v: k for k, v in decoded.items()}
     return decoded
+
 
 def get_pairs(lines: list) -> list:
     pairs = []

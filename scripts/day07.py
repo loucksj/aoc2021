b302c9b69f5e6182646dcc4aafe304427751621c
loucksj <loucksj@gmail.com>
2021-12-28 19:24:09 -0800

    cost_at function reduced with list comprehension

--- scripts/day07.py
+++ scripts/day07.py
@@ -29,15 +29,13 @@ class Crabs():
         return self.cost
 
     def move_next_crab(self, cumulative=False) -> int:
-        at, to = (0, 1) if self.cost_at(0) < self.cost_at(-1) else (-1, -2)
+        startcost, endcost = self.cost_at(0), self.cost_at(-1)
+        at, to = (0, 1) if startcost < endcost else (-1, -2)
+        self.cost += min(startcost, endcost)
         bump = 1 if cumulative else 0
-        self.cost += self.cost_at(at)
         for i, crab in enumerate(self.crabs[at][:-1]):
             self.crabs[to][i + bump] += crab
         del self.crabs[at]
 
     def cost_at(self, index: int) -> int:
-        fuel = 0
-        for i, crab in enumerate(self.crabs[index]):
-            fuel += (i+1) * crab
-        return fuel
+        return sum([(i + 1) * crab for i, crab in enumerate(self.crabs[index])])

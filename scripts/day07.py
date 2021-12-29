7e7e0216270cd4db9d22176976634e5340153ce9
loucksj <loucksj@gmail.com>
2021-12-28 18:58:14 -0800

    fuel cost shared in Class

--- scripts/day07.py
+++ scripts/day07.py
@@ -2,48 +2,47 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    return Crabs(filename).cost()
+    return Crabs(filename).merge()
 
 
 def part_two(filename: str) -> int:
-    return Crabs(filename).cumulative_cost()
+    return Crabs(filename).cumulative_merge()
 
 
 class Crabs():
     def __init__(self, filename: str) -> None:
-        positions = Reader(filename).split_firstline_ints(',')
-        size = max(positions) + 1
+        self.cost = 0
+        crabs = Reader(filename).split_firstline_ints(',')
+        size = max(crabs) + 1
         self.crabs = [[0]*size for _ in range(size)]
         for i in range(len(self.crabs)):
-            self.crabs[i][0] = positions.count(i)
+            self.crabs[i][0] = crabs.count(i)
 
-    def cost(self) -> int:
-        fuel = 0
+    def merge(self) -> int:
         while len(self.crabs) > 1:
             if self.crabs[0][0] < self.crabs[-1][0]:
-                fuel += self.crabs[0][0]
+                self.cost += self.crabs[0][0]
                 self.crabs[1][0] += self.crabs[0][0]
                 del self.crabs[0]
             else:
-                fuel += self.crabs[-1][0]
+                self.cost += self.crabs[-1][0]
                 self.crabs[-2][0] += self.crabs[-1][0]
                 del self.crabs[-1]
-        return fuel
+        return self.cost
 
-    def cumulative_cost(self) -> int:
-        fuel = 0
+    def cumulative_merge(self) -> int:
         while len(self.crabs) > 1:
             if self.cost_at_cumulative(0) < self.cost_at_cumulative(-1):
-                fuel += self.cost_at_cumulative(0)
+                self.cost += self.cost_at_cumulative(0)
                 for i, crab in enumerate(self.crabs[0][:-1]):
                     self.crabs[1][i+1] += crab
                 del self.crabs[0]
             else:
-                fuel += self.cost_at_cumulative(-1)
+                self.cost += self.cost_at_cumulative(-1)
                 for i, crab in enumerate(self.crabs[-1][:-1]):
                     self.crabs[-2][i+1] += crab
                 del self.crabs[-1]
-        return fuel
+        return self.cost
 
     def cost_at_cumulative(self, index: int) -> int:
         fuel = 0

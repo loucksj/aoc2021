b3551fe35a1712778c7be95b4e35040088ccc319
loucksj <loucksj@gmail.com>
2021-12-28 19:32:10 -0800

    merge functions combined

--- scripts/day07.py
+++ scripts/day07.py
@@ -6,35 +6,33 @@ def part_one(filename: str) -> int:
 
 
 def part_two(filename: str) -> int:
-    return Crabs(filename).merge_cumulative()
+    return Crabs(filename).merge(True)
 
 
 class Crabs():
     def __init__(self, filename: str) -> None:
-        self.cost = 0
-        crabs = Reader(filename).split_firstline_ints(',')
-        size = max(crabs) + 1
-        self.crabs = [[0]*size for _ in range(size)]
-        for i in range(len(self.crabs)):
-            self.crabs[i][0] = crabs.count(i)
+        self.crabs = self.make_crabs(Reader(filename).split_firstline_ints(','))
+        self.spent_fuel = 0
 
-    def merge(self) -> int:
-        while len(self.crabs) > 1:
-            self.move_next_crab(False)
-        return self.cost
+    def make_crabs(self, positions: list) -> list:
+        size = max(positions) + 1
+        crabs = [[0]*size for _ in range(size)]
+        for i in range(len(crabs)):
+            crabs[i][0] = positions.count(i)
+        return crabs
 
-    def merge_cumulative(self) -> int:
+    def merge(self, cumulative=False) -> int:
         while len(self.crabs) > 1:
-            self.move_next_crab(True)
-        return self.cost
+            self.move_next_crab(cumulative)
+        return self.spent_fuel
 
     def move_next_crab(self, cumulative=False) -> int:
+        bump = 1 if cumulative else 0
         startcost, endcost = self.cost_at(0), self.cost_at(-1)
         at, to = (0, 1) if startcost < endcost else (-1, -2)
-        self.cost += min(startcost, endcost)
-        bump = 1 if cumulative else 0
-        for i, crab in enumerate(self.crabs[at][:-1]):
-            self.crabs[to][i + bump] += crab
+        self.spent_fuel += min(startcost, endcost)
+        for i, val in enumerate(self.crabs[at][:-1]):
+            self.crabs[to][i + bump] += val
         del self.crabs[at]
 
     def cost_at(self, index: int) -> int:

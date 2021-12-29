283e50332362f8acd89801100678339a3169e936
loucksj <loucksj@gmail.com>
2021-12-28 17:44:01 -0800

    shared crabs matrix

--- scripts/day07.py
+++ scripts/day07.py
@@ -11,47 +11,44 @@ def part_two(filename: str) -> int:
 
 class Crabs():
     def __init__(self, filename: str) -> None:
-        self.positions = Reader(filename).split_firstline_ints(',')
-        self.crabs = [0]*(max(self.positions) + 1)
-        for p in self.positions:
-            self.crabs[p] += 1
+        positions = Reader(filename).split_firstline_ints(',')
+        size = max(positions)+1
+        self.crabs = [[0]*size]
+        for _ in range(1, size):
+            self.crabs.append([0]*size)
+        for p in positions:
+            self.crabs[0][p] += 1
 
     def cost(self) -> int:
         fuel = 0
         start = 0
-        end = len(self.crabs)-1
+        end = len(self.crabs[0])-1
         while start != end:
-            if self.crabs[start] == 0:
+            if self.crabs[0][start] == 0:
                 start += 1
                 continue
-            if self.crabs[end] == 0:
+            if self.crabs[0][end] == 0:
                 end -= 1
                 continue
-            if self.crabs[start] <= self.crabs[end]:
-                fuel += self.crabs[start]
-                self.crabs[start+1] += self.crabs[start]
-                self.crabs[start] = 0
+            if self.crabs[0][start] <= self.crabs[0][end]:
+                fuel += self.crabs[0][start]
+                self.crabs[0][start+1] += self.crabs[0][start]
+                self.crabs[0][start] = 0
             else:
-                fuel += self.crabs[end]
-                self.crabs[end-1] += self.crabs[end]
-                self.crabs[end] = 0
+                fuel += self.crabs[0][end]
+                self.crabs[0][end-1] += self.crabs[0][end]
+                self.crabs[0][end] = 0
         return fuel
 
     def cumulative_cost(self) -> int:
-        size = max(self.positions)+1
-        crabs = [[0]*size]
-        for _ in range(1, size):
-            crabs.append([0]*size)
-        for p in self.positions:
-            crabs[0][p] += 1
         fuel = 0
         start = 0
-        end = len(crabs[0])-1
+        end = len(self.crabs[0])-1
         while start != end:
             startsum = 0
             endsum = 0
             i = 1
-            for line in crabs:
+            for line in self.crabs:
                 startsum += line[start]*i
                 endsum += line[end]*i
                 i += 1
@@ -63,12 +60,12 @@ class Crabs():
                 continue
             if startsum <= endsum:
                 fuel += startsum
-                for i in range(0, len(crabs)-1):
-                    crabs[i+1][start+1] += crabs[i][start]
-                    crabs[i][start] = 0
+                for i in range(0, len(self.crabs)-1):
+                    self.crabs[i+1][start+1] += self.crabs[i][start]
+                    self.crabs[i][start] = 0
             else:
                 fuel += endsum
-                for i in range(0, len(crabs)-1):
-                    crabs[i+1][end-1] += crabs[i][end]
-                    crabs[i][end] = 0
+                for i in range(0, len(self.crabs)-1):
+                    self.crabs[i+1][end-1] += self.crabs[i][end]
+                    self.crabs[i][end] = 0
         return fuel

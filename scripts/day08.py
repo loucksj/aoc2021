d61a7f5c0408a4a62aa81e3bdc60b16408c8e0e5
loucksj <loucksj@gmail.com>
2021-12-29 23:51:33 -0800

    letter sums function

--- scripts/day08.py
+++ scripts/day08.py
@@ -2,7 +2,9 @@ from scripts.main import Reader
 
 # Each number 0-9 has a unique sum of frequencies of its letters a signal.
 # Ex: Number 1 is made of two letters, which appear 17 (8 + 9) times in a signal.
-SUM_KEY = {42:0, 17:1, 34:2, 39:3, 30:4, 37:5, 41:6, 25:7, 49:8, 45:9}
+SUM_KEY = {42: 0, 17: 1, 34: 2, 39: 3,
+           30: 4, 37: 5, 41: 6, 25: 7, 49: 8, 45: 9}
+
 
 def part_one(filename: str) -> int:
     lines = Reader(filename).split_lines(' | ')
@@ -30,11 +32,15 @@ class Decoder():
         return total
 
     def decoded(self, signals: list) -> dict:
-        sums = {}
-        combined = "".join(signals)
-        for signal in signals:
-            sums[signal] = sum(combined.count(letter) for letter in signal)
+        sums = self.letter_sums(signals)
         code = {}
         for signal in signals:
             code[signal] = SUM_KEY[sums[signal]]
         return code
+    
+    def letter_sums(self, signals) -> dict:
+        sums = {}
+        combined = "".join(signals)
+        for signal in signals:
+            sums[signal] = sum(combined.count(letter) for letter in signal)
+        return sums

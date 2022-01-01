e3bd0c4383a37393fdd08eb250e593903b419415
loucksj <loucksj@gmail.com>
2021-12-31 17:42:44 -0800

    added Syntax class to cleanup arguments

--- scripts/day10.py
+++ scripts/day10.py
@@ -10,56 +10,53 @@ INCOMPLETE_POINTS = {')': 1, ']': 2, '}': 3, '>': 4}
 
 
 def part_one(filename: str) -> int:
-    return score_corrupted(filename)
+    return Syntax(filename).score_corrupted()
 
 
 def part_two(filename: str) -> int:
-    return score_incomplete(filename)
+    return Syntax(filename).score_incomplete()
 
 
-def score_corrupted(filename: str) -> int:
-    return sum(CORRUPT_POINTS[char] for char in corrupt_chars(filename))
+class Syntax():
+    def __init__(self, filename: str) -> None:
+        self.lines = Reader(filename).char_lines()
 
+    def score_corrupted(self) -> int:
+        return sum(CORRUPT_POINTS[char] for char in self.corrupt_chars())
 
-def corrupt_chars(filename: str) -> list:
-    return [char for _, char in line_data(filename)[0]]
+    def corrupt_chars(self) -> list:
+        return [char for _, char in self.line_data()[0]]
 
+    def score_incomplete(self) -> int:
+        scores = self.incomplete_scores()
+        middle_index = int((len(scores) - 1) / 2)
+        return sorted(scores)[middle_index]
 
-def score_incomplete(filename: str) -> int:
-    scores = incomplete_scores(filename)
-    middle_index = int((len(scores) - 1) / 2)
-    return sorted(scores)[middle_index]
+    def incomplete_scores(self) -> int:
+        return [self.incomplete_line_score(line) for line in self.incomplete_chars()]
 
+    def incomplete_chars(self) -> list:
+        return [chars for _, chars in self.line_data()[1]]
 
-def incomplete_scores(filename: str) -> int:
-    return [incomplete_line_score(line) for line in incomplete_chars(filename)]
+    def incomplete_line_score(self, line: list):
+        score = 0
+        for error in line:
+            score = score * 5 + INCOMPLETE_POINTS[error]
+        return score
 
-
-def incomplete_chars(filename: str) -> list:
-    return [chars for _, chars in line_data(filename)[1]]
-
-
-def incomplete_line_score(line: list):
-    score = 0
-    for error in line:
-        score = score * 5 + INCOMPLETE_POINTS[error]
-    return score
-
-
-def line_data(filename: str) -> list:
-    lines = Reader(filename).char_lines()
-    corrupted = []
-    incomplete = []
-    for line in lines:
-        stack = []
-        corrupt = False
-        for char in line:
-            if char in PAIRS.keys():
-                stack.append(PAIRS[char])
-            elif char != stack.pop():
-                corrupted.append((line, char))
-                corrupt = True
-                break
-        if not corrupt:
-            incomplete.append((line, list(reversed(stack))))
-    return (corrupted, incomplete)
+    def line_data(self) -> list:
+        corrupted = []
+        incomplete = []
+        for line in self.lines:
+            stack = []
+            corrupt = False
+            for char in line:
+                if char in PAIRS.keys():
+                    stack.append(PAIRS[char])
+                elif char != stack.pop():
+                    corrupted.append((line, char))
+                    corrupt = True
+                    break
+            if not corrupt:
+                incomplete.append((line, list(reversed(stack))))
+        return (corrupted, incomplete)

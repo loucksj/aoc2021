b7de2a6fd134dfeda836074215d61255a37fe90d
loucksj <loucksj@gmail.com>
2021-12-31 19:00:16 -0800

    simplified with energize_all

--- scripts/day11.py
+++ scripts/day11.py
@@ -7,35 +7,36 @@ DIRECTIONS = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
 
 
 def part_one(filename: str) -> int:
-    return Board(filename).step(100)
+    return Board(filename).step(100).flashes
 
 
 def part_two(filename: str) -> int:
-    return Board(filename).allflash()
+    return Board(filename).allflash_at()
 
 
 class Board:
     def __init__(self, filename: str):
         self.rows = Reader(filename).integer_lines()
+        self.flashes = 0
 
     def step(self, times: int) -> int:
-        flashes = 0
         for _ in range(times):
-            for row in range(10):
-                for col in range(10):
-                    self.energize(row, col)
-            flashes += self.reset_nines()
-        return flashes
+            self.energize_all()
+        return self
 
-    def allflash(self) -> int:
+    def allflash_at(self) -> int:
         step = 0
         while True:
             step += 1
-            for row in range(10):
-                for col in range(10):
-                    self.energize(row, col)
-            if self.reset_nines() == 100:
+            self.energize_all()
+            if sum(sum(col for col in row) for row in self.rows) == 0:
                 return step
+
+    def energize_all(self):
+        for row in range(10):
+            for col in range(10):
+                self.energize(row, col)
+        self.reset_nines()
 
     def energize(self, row: int, col: int):
         if 0 <= row < 10 and 0 <= col < 10:
@@ -44,11 +45,9 @@ class Board:
                 for r, c in DIRECTIONS:
                     self.energize(row + r, col + c)
 
-    def reset_nines(self) -> int:
-        resets = 0
+    def reset_nines(self):
         for row in range(10):
             for col in range(10):
                 if self.rows[row][col] > 9:
                     self.rows[row][col] = 0
-                    resets += 1
-        return resets
+                    self.flashes += 1

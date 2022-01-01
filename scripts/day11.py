3b83d1034134150f27917af389345fcc2868879b
loucksj <loucksj@gmail.com>
2021-12-31 18:38:13 -0800

    simplified part one with step

--- scripts/day11.py
+++ scripts/day11.py
@@ -4,24 +4,17 @@ from scripts.main import Reader
 DIRECTIONS = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
 
 def part_one(filename: str) -> int:
-    board = Board(filename)
-    flashes = 0
-    for _ in range(0, 100):
-        for row in range(0, 10):
-            for col in range(0, 10):
-                board.energize(row, col)
-        flashes += board.reset()
-    return flashes
+    return Board(filename).step(100)
 
 def part_two(filename: str) -> int:
     board = Board(filename)
     step = 0
     while True:
         step += 1
-        for row in range(0, 10):
-            for col in range(0, 10):
+        for row in range(10):
+            for col in range(10):
                 board.energize(row, col)
-        if board.reset() == 100:
+        if board.reset_nines() == 100:
             return step
 
 class Board:
@@ -29,6 +22,15 @@ class Board:
         self.rows = []
         for line in Reader(filename).lines():
             self.rows.append([int(x) for x in line])
+    
+    def step(self, times: int) -> int:
+        flashes = 0
+        for _ in range(times):
+            for row in range(10):
+                for col in range(10):
+                    self.energize(row, col)
+            flashes += self.reset_nines()
+        return flashes
 
     def energize(self, row: int, col: int):
         if 0 <= row < 10 and 0 <= col < 10:
@@ -37,11 +39,11 @@ class Board:
                 for r, c in DIRECTIONS:
                     self.energize(row + r, col + c)
     
-    def reset(self) -> int:
-        flashes = 0
-        for row in range(0, 10):
-            for col in range(0, 10):
+    def reset_nines(self) -> int:
+        resets = 0
+        for row in range(10):
+            for col in range(10):
                 if self.rows[row][col] > 9:
                     self.rows[row][col] = 0
-                    flashes += 1
-        return flashes
+                    resets += 1
+        return resets

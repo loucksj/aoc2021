b20cb3447ebe6d63e262341734b2aa13dc017ec8
loucksj <loucksj@gmail.com>
2021-12-31 21:29:20 -0800

    naming and readability

--- scripts/day12.py
+++ scripts/day12.py
@@ -1,5 +1,6 @@
 from scripts.main import Reader
 
+
 def part_one(filename: str) -> int:
     return Caves(filename).count_paths()
 
@@ -20,20 +21,20 @@ class Caves:
             self.caves = []
             self.is_big = True if self.name.isupper() else False
 
-    def count_paths(self, boost=False, cave=[], path=[]) -> int:
+    def count_paths(self, boost=False, cave=[], visited=[]) -> int:
         if cave == []:
             cave = self.get_cave('start')
         if cave.name == 'end':
             return 1
         count = 0
-        path.append(cave.name)
+        visited.append(cave)
         for to_cave in cave.caves:
             if to_cave.name == 'start':
                 continue
-            if to_cave.name in path and not to_cave.is_big and boost:
-                count += self.count_paths(False, to_cave, path.copy())
-            if to_cave.name not in path or to_cave.is_big:
-                count += self.count_paths(boost, to_cave, path.copy())
+            if to_cave in visited and not to_cave.is_big and boost:
+                count += self.count_paths(False, to_cave, visited.copy())
+            if to_cave not in visited or to_cave.is_big:
+                count += self.count_paths(boost, to_cave, visited.copy())
         return count
 
     def add_connection(self, from_name: str, to_name: str):

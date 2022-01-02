b42afff86bbb2701067737c0015b17429c688edd
loucksj <loucksj@gmail.com>
2022-01-01 18:15:08 -0800

    naming

--- scripts/day14.py
+++ scripts/day14.py
@@ -6,49 +6,41 @@ def part_one(filename: str) -> int:
 
 
 def part_two(filename: str) -> int:
-    lines = Reader(filename).lines()
+    polymer = Polymer(filename)
 
-    polymer = lines.pop(0)
-    lines.pop(0)
-
-    rules = {}
-    for line in lines:
-        pair = line.split(' -> ')
-        rules[pair[0]] = pair[1]
-
-    pairs = get_pairs(polymer)
+    pairs = get_pairs(polymer.chain)
     for _ in range(40):
-        pairs = pair_insert(rules, pairs)
+        pairs = pair_insert(polymer.rules, pairs)
 
-    return pair_score(pairs, polymer[0], polymer[-1])
+    return pair_score(pairs, polymer.chain[0], polymer.chain[-1])
 
 
 class Polymer():
     def __init__(self, filename: str) -> None:
         lines = Reader(filename).halves_lined()
-        self.template = lines[0][0]
-        self.pairs = dict(line.split(' -> ') for line in lines[1])
+        self.chain = lines[0][0]
+        self.rules = dict(line.split(' -> ') for line in lines[1])
 
     def do_steps(self, steps):
         for _ in range(steps):
-            self.template = self.insert()
+            self.chain = self.insert()
         return self
 
     def insert(self) -> str:
         new_polymer = ''
-        for i in range(len(self.template)-1):
-            a = self.template[i]
-            b = self.template[i+1]
+        for i in range(len(self.chain)-1):
+            a = self.chain[i]
+            b = self.chain[i+1]
             ab = a+b
             new_polymer += a
-            if ab in self.pairs.keys():
-                new_polymer += self.pairs[ab]
-        new_polymer += self.template[-1]
+            if ab in self.rules.keys():
+                new_polymer += self.rules[ab]
+        new_polymer += self.chain[-1]
         return new_polymer
 
     def score(self) -> int:
         counts = {}
-        for s in self.template:
+        for s in self.chain:
             if s in counts.keys():
                 counts[s] += 1
             else:
@@ -96,7 +88,7 @@ def pair_insert(rules: dict, pairs: dict) -> dict:
 
 def get_pairs(polymer: str) -> dict:
     pairs = {}
-    for i in range(0, len(polymer)-1):
+    for i in range(len(polymer)-1):
         a = polymer[i]
         b = polymer[i+1]
         ab = a+b

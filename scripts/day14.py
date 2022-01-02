21a8778a54bc8d5a6e3840239f6fd98d0220ca0a
loucksj <loucksj@gmail.com>
2022-01-01 19:18:49 -0800

    readability

--- scripts/day14.py
+++ scripts/day14.py
@@ -2,11 +2,11 @@ from scripts.main import Reader
 
 
 def part_one(filename: str) -> int:
-    return Polymer(filename).do_steps(10).score()
+    return Polymer(filename).steps(10).score()
 
 
 def part_two(filename: str) -> int:
-    return Polymer(filename).do_steps(40).score()
+    return Polymer(filename).steps(40).score()
 
 
 class Polymer():
@@ -18,7 +18,7 @@ class Polymer():
         self.rules = dict(line.split(' -> ') for line in lines[1])
         self.pair_counts = self.count_pairs()
 
-    def do_steps(self, steps):
+    def steps(self, steps):
         for _ in range(steps):
             self.step()
         return self
@@ -28,31 +28,29 @@ class Polymer():
         return max(scores) - min(scores)
 
     def letter_scores(self):
-        # Each letter is part of two pairs, so its count is halved...
         scores = {}
-        for key in self.pair_counts.keys():
-            for char in key:
-                scores[char] = scores.get(char, 0) + self.pair_counts[key] / 2
+        for pair in self.pair_counts:
+            for char in pair:
+                # Each letter is part of two pairs, so its count is halved...
+                scores[char] = scores.get(char, 0) + self.pair_counts[pair] / 2
         # ...except the first and last letter, which are un-halved
-        first, last = self.original_chain[0], self.original_chain[-1]
-        scores[first] += 0.5
-        scores[last] += 0.5
+        scores[self.original_chain[0]] += 0.5
+        scores[self.original_chain[-1]] += 0.5
         return scores.values()
 
     def step(self) -> dict:
-        next_pairs = {}
-        for pair in self.pair_counts:
-            if pair in self.rules.keys():
-                count = self.pair_counts[pair]
-                left = pair[0] + self.rules[pair]
-                right = self.rules[pair] + pair[1]
-                next_pairs[left] = next_pairs.get(left, 0) + count
-                next_pairs[right] = next_pairs.get(right, 0) + count
-        self.pair_counts = next_pairs
+        pair_counts = {}
+        for pair in [pair for pair in self.pair_counts if pair in self.rules]:
+            left = pair[0] + self.rules[pair]
+            right = self.rules[pair] + pair[1]
+            count = self.pair_counts[pair]
+            pair_counts[left] = pair_counts.get(left, 0) + count
+            pair_counts[right] = pair_counts.get(right, 0) + count
+        self.pair_counts = pair_counts
 
     def count_pairs(self) -> dict:
         pairs = {}
-        for i in range(len(self.original_chain)-1):
+        for i, _ in enumerate(self.original_chain[:-1]):
             pair = self.original_chain[i] + self.original_chain[i + 1]
             pairs[pair] = pairs.get(pair, 0) + 1
         return pairs

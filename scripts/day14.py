d9c6714028e097d06c3242e3944c88c90580164a
loucksj <loucksj@gmail.com>
2022-01-01 18:39:26 -0800

    using fastest method, removed alternate method from part one

--- scripts/day14.py
+++ scripts/day14.py
@@ -6,51 +6,23 @@ def part_one(filename: str) -> int:
 
 
 def part_two(filename: str) -> int:
-    return Polymer(filename).do_steps_paired(40).pair_score()
+    return Polymer(filename).do_steps(40).score()
 
 
 class Polymer():
     def __init__(self, filename: str) -> None:
         lines = Reader(filename).halves_lined()
-        self.chain = lines[0][0]
+        self.original_chain = lines[0][0]
         self.rules = dict(line.split(' -> ') for line in lines[1])
         self.pairs = self.get_pairs()
 
     def do_steps(self, steps):
         for _ in range(steps):
-            self.chain = self.insert()
+            self.process()
         return self
 
-    def do_steps_paired(self, steps):
-        for _ in range(steps):
-            self.pair_insert()
-        return self
-
-    def insert(self) -> str:
-        new_polymer = ''
-        for i in range(len(self.chain)-1):
-            a = self.chain[i]
-            b = self.chain[i+1]
-            ab = a+b
-            new_polymer += a
-            if ab in self.rules.keys():
-                new_polymer += self.rules[ab]
-        new_polymer += self.chain[-1]
-        return new_polymer
-
-    def score(self) -> int:
-        counts = {}
-        for s in self.chain:
-            if s in counts.keys():
-                counts[s] += 1
-            else:
-                counts[s] = 1
-        least = min(counts.values())
-        most = max(counts.values())
-        return most - least
-
-    def pair_score(self):
-        first, last = self.chain[0], self.chain[-1]
+    def score(self):
+        first, last = self.original_chain[0], self.original_chain[-1]
         scores = {}
         for key in self.pairs.keys():
             magnitude = self.pairs[key]
@@ -66,7 +38,7 @@ class Polymer():
         score = largest - smallest
         return int(score)
 
-    def pair_insert(self) -> dict:
+    def process(self) -> dict:
         new_pairs = {}
         for pair in self.pairs:
             if pair in self.rules.keys():
@@ -86,9 +58,9 @@ class Polymer():
 
     def get_pairs(self) -> dict:
         pairs = {}
-        for i in range(len(self.chain)-1):
-            a = self.chain[i]
-            b = self.chain[i+1]
+        for i in range(len(self.original_chain)-1):
+            a = self.original_chain[i]
+            b = self.original_chain[i+1]
             ab = a+b
             if ab in pairs.keys():
                 pairs[ab] += 1

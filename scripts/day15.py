e86db265d679e14b8712ec1d2f90e7503bed6cb0
loucksj <loucksj@gmail.com>
2022-01-01 22:22:14 -0800

    better Reader use, split tuple into risks map

--- scripts/day15.py
+++ scripts/day15.py
@@ -9,12 +9,9 @@ def part_two(filename: str) -> int:
 class Map():
     def __init__(self, filename: str):
         self.point = [0, 0]
-        self.rows = []
-        self.open = [[0, 0]]
-        for i, line in enumerate(Reader(filename).lines()):
-            self.rows.append([])
-            for char in line:
-                self.rows[i].append([int(char), -1])
+        self.rows = Reader(filename).integer_lines()
+        self.risks = [[-1 for _ in row] for row in self.rows]
+        self.current = [[0, 0]]
 
     def expand(self):
         height = len(self.rows)
@@ -24,37 +21,38 @@ class Map():
             new_map.append([])
             for col in range(5*width):
                 if row < height and col < width:
-                    new_map[row].append(self.rows[row][col].copy())
+                    new_map[row].append(self.rows[row][col])
                 elif row >= height:
-                    new_map[row].append(new_map[row-height][col].copy())
+                    new_map[row].append(new_map[row-height][col])
                 else:
-                    new_map[row].append(new_map[row][col-width].copy())
+                    new_map[row].append(new_map[row][col-width])
                 if row >= height or col >= width:
-                    new_map[row][col][0] += 1
-                    if new_map[row][col][0] > 9:
-                        new_map[row][col][0] = 1
+                    new_map[row][col] += 1
+                    if new_map[row][col] > 9:
+                        new_map[row][col] = 1
         self.rows = new_map
+        self.risks = [[-1 for _ in row] for row in self.rows]
         return self
 
     def find_path(self):
         height = len(self.rows)
         width = len(self.rows[0])
-        self.rows[0][0][1] = 0
+        self.risks[0][0] = 0
         while True:
-            self.open.remove([self.point[0], self.point[1]])
+            self.current.remove([self.point[0], self.point[1]])
             self.update()
             self.move()
             if self.point[0] == height-1 and self.point[1] == width-1:
-                return self.rows[height-1][width-1][1]
+                return self.risks[height-1][width-1]
 
     def move(self) -> list:
-        self.rows[self.point[0]][self.point[1]][1] = 0
+        self.risks[self.point[0]][self.point[1]] = 0
         smallest = 0
-        for point in self.open:
-            d = self.rows[point[0]][point[1]][1]
+        for point in self.current:
+            d = self.risks[point[0]][point[1]]
             if d > 0 and (smallest == 0 or d < smallest):
                 self.point = [point[0], point[1]]
-                smallest = self.rows[self.point[0]][self.point[1]][1]
+                smallest = self.risks[self.point[0]][self.point[1]]
 
     def update(self):
         directions = [[-1, 0], [+1, 0], [0, -1], [0, +1]] #up, down, left, right
@@ -63,11 +61,11 @@ class Map():
             col = self.point[1]+direction[1]
             if row < 0 or row >= len(self.rows) or col < 0 or col >= len(self.rows[0]):
                 continue #edge
-            risk = self.rows[row][col][1]
+            risk = self.risks[row][col]
             if risk == 0:
                 continue #visited
-            distance = self.rows[row][col][0] + self.rows[self.point[0]][self.point[1]][1]
+            distance = self.rows[row][col] + self.risks[self.point[0]][self.point[1]]
             if risk == -1 or distance < risk:
                 if risk == -1:
-                    self.open.append([row, col])
-                self.rows[row][col][1] = distance
+                    self.current.append([row, col])
+                self.risks[row][col] = distance

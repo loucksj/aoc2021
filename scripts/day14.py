4057726ef33500d4c861a3cc4335cb3c36922b6d
loucksj <loucksj@gmail.com>
2022-01-01 18:22:32 -0800

    part two functions moved into Polymer class

--- scripts/day14.py
+++ scripts/day14.py
@@ -8,11 +8,11 @@ def part_one(filename: str) -> int:
 def part_two(filename: str) -> int:
     polymer = Polymer(filename)
 
-    pairs = get_pairs(polymer.chain)
+    pairs = polymer.get_pairs()
     for _ in range(40):
-        pairs = pair_insert(polymer.rules, pairs)
+        pairs = polymer.pair_insert(pairs)
 
-    return pair_score(pairs, polymer.chain[0], polymer.chain[-1])
+    return polymer.pair_score(pairs)
 
 
 class Polymer():
@@ -49,51 +49,49 @@ class Polymer():
         most = max(counts.values())
         return most - least
 
+    def pair_score(self, pairs: dict):
+        first, last = self.chain[0], self.chain[-1]
+        scores = {}
+        for key in pairs.keys():
+            magnitude = pairs[key]
+            for s in key:
+                if s in scores:
+                    scores[s] += magnitude / 2
+                else:
+                    scores[s] = magnitude / 2
+        scores[first] += 0.5
+        scores[last] += 0.5
+        largest = max(scores.values())
+        smallest = min(scores.values())
+        score = largest - smallest
+        return int(score)
 
-def pair_score(pairs: dict, first: str, last: str):
-    scores = {}
-    for key in pairs.keys():
-        magnitude = pairs[key]
-        for s in key:
-            if s in scores:
-                scores[s] += magnitude / 2
+    def pair_insert(self, pairs: dict) -> dict:
+        new_pairs = {}
+        for pair in pairs:
+            if pair in self.rules.keys():
+                magnitude = pairs[pair]
+                b = self.rules[pair]
+                ab = pair[0] + b
+                if ab in new_pairs:
+                    new_pairs[ab] += magnitude
+                else:
+                    new_pairs[ab] = magnitude
+                bc = b + pair[1]
+                if bc in new_pairs:
+                    new_pairs[bc] += magnitude
+                else:
+                    new_pairs[bc] = magnitude
+        return new_pairs
+
+    def get_pairs(self) -> dict:
+        pairs = {}
+        for i in range(len(self.chain)-1):
+            a = self.chain[i]
+            b = self.chain[i+1]
+            ab = a+b
+            if ab in pairs.keys():
+                pairs[ab] += 1
             else:
-                scores[s] = magnitude / 2
-    scores[first] += 0.5
-    scores[last] += 0.5
-    largest = max(scores.values())
-    smallest = min(scores.values())
-    score = largest - smallest
-    return int(score)
-
-
-def pair_insert(rules: dict, pairs: dict) -> dict:
-    new_pairs = {}
-    for pair in pairs:
-        if pair in rules.keys():
-            magnitude = pairs[pair]
-            b = rules[pair]
-            ab = pair[0] + b
-            if ab in new_pairs:
-                new_pairs[ab] += magnitude
-            else:
-                new_pairs[ab] = magnitude
-            bc = b + pair[1]
-            if bc in new_pairs:
-                new_pairs[bc] += magnitude
-            else:
-                new_pairs[bc] = magnitude
-    return new_pairs
-
-
-def get_pairs(polymer: str) -> dict:
-    pairs = {}
-    for i in range(len(polymer)-1):
-        a = polymer[i]
-        b = polymer[i+1]
-        ab = a+b
-        if ab in pairs.keys():
-            pairs[ab] += 1
-        else:
-            pairs[ab] = 1
-    return pairs
+                pairs[ab] = 1
+        return pairs
